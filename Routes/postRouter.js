4f00b4c4416fc122011927d57dfcb4d92a281caf
Adetunji <ashennaike@gmail.com>
2019-04-24 16:14:51 -0400

    reusing error components

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -7,7 +7,16 @@ const Posts = require('../data/helpers/postDb');
 //define the router
 const router = express.Router();
 
+//make a constant reply for 404 & 500
+const sendError = (msg, res) => {
+  res.status(500);
+  res.json({ errorMessage: `${msg}` })
+};
 
+const sendMissingID = (res) => {
+  res.status(404);
+  res.json({ errorMessage: 'The post with the specified ID does not exist.' });
+}
 
 //export
 module.exports = router;
--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -7,6 +7,22 @@ const Users = require('../data/helpers/userDb');
 //define the router
 const router = express.Router();
 
+//make a constant reply for 404 & 500
+const sendError = (msg, res) => {
+  res.status(500);
+  res.json({ errorMessage: `${msg}` })
+};
+
+const sendMissing = (res) => {
+  res.status(404);
+  res.json({ errorMessage: 'The User does not exist.' });
+}
+
+//make crud endpoints
+//get request
+router.get('/', (req, res) => {
+
+})
 
 
 //export
