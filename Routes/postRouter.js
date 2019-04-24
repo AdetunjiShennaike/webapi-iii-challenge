882c666bddc90d3081f5889ef086a2e31959f598
Adetunji <ashennaike@gmail.com>
2019-04-24 17:14:13 -0400

    finished both routers, checking which ones work, the gets work for both running through the rest

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -18,5 +18,123 @@ const sendMissingID = (res) => {
   res.json({ errorMessage: 'The post with the specified ID does not exist.' });
 }
 
+//make crud endpoints
+//get request
+router.get('/', (req, res) => {
+  Posts
+  .get()
+  .then( post => {
+    res.status(200).json(post);
+  })
+  .catch( err => {
+    return sendError( 'Post information Unavailable at this moment', err );
+  })
+})
+
+//get post by id
+router.get('/:id', (req, res) => {
+  //define id
+  const ID = req.params.id
+  Posts
+  .getById(ID)
+  .then( post => {
+    console.log(post);
+    if (post === undefined) {
+      return sendMissingID(res);
+    }
+    else{
+      return res.status(200).json(post);
+    }
+  })
+  .catch( err => {
+    return sendError( 'post information Unavailable at this moment', err );
+  })
+})
+
+//update post
+router.put('/:id', (req, res) => {
+  //define id 
+  const ID = req.params.id
+
+  //define req.body
+  const name = req.body;
+  const post = { name };
+
+  //check the req body
+  if(!name) { 
+    return res.status(400).json({ error: 'Please provide the NEW post name' });
+  }
+  Posts
+  .update(ID, post)
+  .then( person => {
+    if (person === undefined) {
+      return sendMissingID(res);
+    }
+    else{
+      newPost = { ...person, post }
+      return res.status(201).json(newPost);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+})
+
+//delete post
+router.delete('/:id', (req, res) => {
+  //set id
+  const ID = req.params.id
+  //grab post information 
+  Posts.findById(ID)
+  .then( post => { 
+    if (post === undefined) {
+      return sendMissingID(res);
+    }
+    else{
+      return res.status(200).json(post);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+  //delete the post
+  Posts
+  .remove(ID)
+  .then( post => { 
+    if (post === undefined) {
+      return sendMissingID(res);
+    }
+    else{
+      return res.status(200).json(post);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+})
+
+//new post
+router.post('/', (req, res) => {
+  //define req.body
+  const name = req.body;
+  const post = { name };
+
+  //check the req body
+  if(!name) { 
+    return res.status(400).json({ error: 'Please provide the NEW post name' });
+  }
+  Posts
+  .insert(post)
+  .then( person => {
+    let newPost = { ...person, post }
+    res.status(200).json(newPost);
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+})
+
+
+
 //export
 module.exports = router;
--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -21,7 +21,135 @@ const sendMissing = (res) => {
 //make crud endpoints
 //get request
 router.get('/', (req, res) => {
+  Users
+  .get()
+  .then( user => {
+    res.status(200).json(user);
+  })
+  .catch( err => {
+    return sendError( 'User information Unavailable at this moment', err );
+  })
+})
 
+//get user by id
+router.get('/:id', (req, res) => {
+  //define id
+  const ID = req.params.id
+  Users
+  .getById(ID)
+  .then( user => {
+    if (user === undefined) {
+      return sendMissing(res);
+    }
+    else{
+      return res.status(200).json(user);
+    }
+  })
+  .catch( err => {
+    return sendError( 'User information Unavailable at this moment', err );
+  })
+})
+
+//get users post data
+router.get(':id/posts', (req, res) => {
+  //define id
+  const ID = req.params.id
+  Users
+  .getUserPosts(ID)
+  .then( user => {
+    if (user === undefined) {
+      return sendMissing(res);
+    }
+    else{
+      return res.status(200).json(user);
+    }
+  })
+  .catch( err => {
+    return sendError( 'User information Unavailable at this moment', err );
+  })
+})
+
+//update user
+router.put('/:id', (req, res) => {
+  //define id 
+  const ID = req.params.id
+
+  //define req.body
+  const name = req.body;
+  const user = { name };
+
+  //check the req body
+  if(!name) { 
+    return res.status(400).json({ error: 'Please provide the NEW user name' });
+  }
+  Users
+  .update(ID, user)
+  .then( person => {
+    if (person === undefined) {
+      return sendMissing(res);
+    }
+    else{
+      newUser = { ...person, user }
+      return res.status(201).json(newUser);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+})
+
+//delete user
+router.delete('/:id', (req, res) => {
+  //set id
+  const ID = req.params.id
+  //grab user information 
+  Users.findById(ID)
+  .then( user => { 
+    if (user === undefined) {
+      return sendMissing(res);
+    }
+    else{
+      return res.status(200).json(user);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+  //delete the user
+  Users
+  .remove(ID)
+  .then( user => { 
+    if (user === undefined) {
+      return sendMissing(res);
+    }
+    else{
+      return res.status(200).json(user);
+    }
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
+})
+
+//new user
+router.post('/', (req, res) => {
+  //define req.body
+  const name = req.body;
+  const user = { name };
+
+  //check the req body
+  if(!name) { 
+    return res.status(400).json({ error: 'Please provide the NEW user name' });
+  }
+  Users
+  .insert(user)
+  .then( person => {
+    let newUser = { ...person, user }
+    res.status(200).json(newUser);
+  })
+  .catch( err => {
+    return sendError( 'This function is currently unavailable', err );
+  })
 })
 
 
