e6799337431b1dbf6cf52d9b8dd4d065320a55ab
Adetunji <ashennaike@gmail.com>
2019-04-24 15:52:20 -0400

    basic routes setup without endpoints, backend is now working

--- Routes/postRouter.js
+++ Routes/postRouter.js
@@ -1,1 +1,13 @@
-const server = require('')
+//import express
+const express = require('express');
+
+//import database
+const Posts = require('../data/helpers/postDb');
+
+//define the router
+const router = express.Router();
+
+
+
+//export
+module.exports = router;
---
+++ Routes/userRouter.js
@@ -0,0 +1,13 @@
+//import express
+const express = require('express');
+
+//import database
+const Users = require('../data/helpers/userDb');
+
+//define the router
+const router = express.Router();
+
+
+
+//export
+module.exports = router;
